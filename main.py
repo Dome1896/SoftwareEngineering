3d4cae2641992390346bdafb5285cfcbeeaf19c8
Lukas Schreiber <lukas.schreiber@iu-study.org>
2024-06-06 14:55:52 +0200

    ignore pycache

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from kivy.uix.label import Label
 from database import Database
 from card import Card
 
-# class MyApp(App):
-#     def build(self):
-#         # Erstelle ein Label-Widget
-#         return Label(text="Hello, Kivy!")
+class MyApp(App):
+     def build(self):
+        # Erstelle ein Label-Widget
+        return Label(text="Hello, Kivy!")
 db = Database()
 card = Card( "tst", "tsest", "testcate")
 db.setDataToDB(card)
@@ -21,8 +21,8 @@ filtered_card = db.getDataFromTableWithFilter("Cardholder", "id", "17")
 print(filtered_card)
 
 hs = 0
-# Starte die Anwendung
-# if __name__ == '__main__':
 
-#     MyApp().run()
+if __name__ == '__main__':
+
+     MyApp().run()
     
