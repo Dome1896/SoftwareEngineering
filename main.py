bf89fd7dac55256b00ac2c04fbd3681f4e710b60
Tjark <tjark.ruppe@vodafone.com>
2024-06-13 10:01:33 +0200

    main.py comments

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+# Importiere die notwendigen Module aus Kivy, um eine einfache GUI-Anwendung zu erstellen.
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+
+# Importiere eigene Module, die zur Verwaltung von Datenbanken und Karten verwendet werden.
 from database import Database
 from card import Card
 from controller import Controller
 
+# Erstelle eine Instanz der Datenbankklasse.
 db = Database()
+
+# Erstelle eine Karte mit Beispielwerten und füge sie zur Datenbank hinzu.
 card = Card("tst", "tsest", "testcate")
 db.setDataToDB(card)
-# Alle Daten aus der Tabelle abrufen
+
+# Rufe alle Daten aus der Tabelle "Cardholder" ab und drucke sie aus.
 all_users = db.getAllDataFromOneTable("Cardholder")
 print(all_users)
 
