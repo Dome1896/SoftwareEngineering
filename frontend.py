6ef4617b15141320a3342eb759e77570a3b7c7b7
DESKTOP-O1PQJSG\marco <levin.bresching@gmail.com>
2024-06-13 14:33:58 +0200

    Toolbar changed Button adds folder to toolbar with icon and label

--- frontend.py
+++ frontend.py
@@ -1,5 +1,8 @@
 from kivy.app import App
+from kivy.properties import StringProperty, ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.animation import Animation
 from kivy.lang import Builder
@@ -9,21 +12,22 @@ from kivy.uix.scrollview import ScrollView
 
 from controller import Controller
 
+
 class MyFloatLayout(FloatLayout):
     toolbar_expanded = True  # Zustand der Toolbar
 
+    # def on_kv_post(self, *args):
+    # self.add_category_buttons()
 
-    def on_kv_post(self, *args):
-        self.add_category_buttons()
+    #def add_category_buttons(self):
+    #categories = self.get_categories()
+    #toolbar = self.ids.category_box
+    #toolbar.clear_widgets()
 
-    def add_category_buttons(self):
-        categories = self.get_categories()
-        toolbar = self.ids.category_box
-        toolbar.clear_widgets()
-
-        for category in categories:
-            btn = Button(text=category, size_hint_y=None, height=40, on_release=lambda *args: self.get_all_cards_for_category(category))
-            toolbar.add_widget(btn)
+    # for category in categories:
+    # btn = Button(text=category, size_hint_y=None, height=40,
+    #    on_release=lambda *args: self.get_all_cards_for_category(category))
+    #toolbar.add_widget(btn)
 
     def get_categories(self):
         return Controller.getAllCategories()
@@ -52,7 +56,7 @@ class MyFloatLayout(FloatLayout):
         self.show_popup()
 
     def show_popup(self):
-        show = P()
+        show = PopupAddCard()
         popupWindow = Popup(title="Add Flashcard", content=show, size_hint=(None, None), size=(400, 400))
 
         def save_data(instance):
@@ -65,17 +69,57 @@ class MyFloatLayout(FloatLayout):
         show.ids.addCard.bind(on_release=save_data)
         popupWindow.open()
 
+    def show_popuptoolbar(self):
+        self.show_popup_folder()
+
+    def show_popup_folder(self):
+        show = PopupToolbar(parent_widget=self)
+        popupWindow = Popup(title="Add Folder", content=show, size_hint=(None, None), size=(200, 200),
+                            pos_hint={'center_x': 0.1, 'center_y': 0.2})
+        popupWindow.open()
+
+    def add_folder_to_toolbar(self, folder_name):
+        # Neues Folder-Widget erstellen
+        folder = Folder(folder_name=folder_name)
+        self.ids.folder_box.add_widget(folder, index=0)
+
+
+    def add_content_to_folder(self, folder_name, content):
+        for folder in self.ids.folder_box.children:
+            if isinstance(folder, Folder) and folder.folder_name == folder_name:
+                folder.add_content(content)
+                break
+
+    def select_folder(self, instance):
+        self.selected_folder = instance
+        print(f"Selected folder: {instance.folder_name}")
+
+    def move_text_to_folder(self):
+        if self.selected_folder:
+            text = self.ids.text_input.text
+            if text:
+                self.selected_folder.add_content(text)
+                self.ids.text_input.text = ''
+            else:
+                print("No text to move")
+        else:
+            print("No folder selected")
+
+
 class WindowManager(ScreenManager):
     pass
 
-class P(FloatLayout):
+
+class PopupAddCard(FloatLayout):
     pass
+
 
 class FirstWindow(Screen, MyFloatLayout):
     cardIndex = 0
+
     def __init__(self, **kwargs):
         super(FirstWindow, self).__init__(**kwargs)
-  # Initialize cardList here
+        # Initialize cardList here
         self.show_answer = False
         # self.add_category_buttons()
 
@@ -128,13 +172,49 @@ class FirstWindow(Screen, MyFloatLayout):
         self.ids.ratingMiddle.opacity = 0
         self.ids.ratingGood.opacity = 0
 
+
+class PopupToolbar(FloatLayout):
+    folder_name_input = ObjectProperty(None)
+
+    def __init__(self, parent_widget, **kwargs):
+        super(PopupToolbar, self).__init__(**kwargs)
+        self.parent_widget = parent_widget
+
+    def create_folder(self):
+        folder_name = self.folder_name_input.text
+        if folder_name:
+            self.parent_widget.add_folder_to_toolbar(folder_name)
+            self.folder_name_input.text = ''
+        else:
+            print("No folder name provided.")
+
+
+class Folder(BoxLayout):
+    folder_name = StringProperty("")
+
+    def __init__(self, folder_name, **kwargs):
+        super(Folder, self).__init__(**kwargs)
+        self.folder_name = folder_name
+
+    def add_content(self, content):
+        self.ids.content.add_widget(Label(text=content, size_hint_y=None, height=30))
+
+    def change_icon(self):
+        # Die Icons sollen sich abwechseln
+        current_icon = self.ids.folder_icon.source
+        new_icon = 'folder.png' if current_icon == 'folderclosed.png' else 'folderclosed.png'
+        self.ids.folder_icon.source = new_icon
+
+
 class SecondWindow(Screen):
     pass
+
 
 class FirstApp(App):
     def build(self):
         kv = Builder.load_file('first_window.kv')
         return kv
+
 
 if __name__ == "__main__":
     FirstApp().run()
