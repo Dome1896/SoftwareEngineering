e160902cf9fe02280b015d1b7b6deeff5bcc6879
Tjark <tjark.ruppe@vodafone.com>
2024-06-20 14:12:58 +0200

    controller kommentiert

--- controller.py
+++ controller.py
@@ -1,44 +1,62 @@
+# Importiert die notwendigen Module und Klassen aus Kivy.
 from kivy.app import App
 from kivy.lang import Builder
+# Importiert die benutzerdefinierten Module Database und Card.
 from database import Database
 from card import Card
 
+# Lädt die Kivy-Datei 'my.kv', die die UI-Definitionen enthält.
 Builder.load_file('my.kv')
 
+# Definiert die Klasse Controller, die von Kivy's App-Klasse erbt.
 class Controller(App):
 
+    # Erstellt eine Klassenvariable 'db', die eine Instanz der Database-Klasse ist.
     db = Database()
 
-    # erstellen
+    # Definiert eine Klassenmethode zum Erstellen einer neuen Karte.
     @classmethod
     def createCard(cls, question: str, answer: str, category: str):
+        # Erstellt eine neue Karte mit den angegebenen Attributen.
         card = Card(question, answer, category)
+        # Speichert die Karte in der Datenbank.
         cls.db.setDataToDB(card)
 
+    # Definiert eine Klassenmethode, die alle Karten für eine bestimmte Kategorie zurückgibt.
     @classmethod
     def getAllCardsForCategory(cls, category: str):
+        # Initialisiert eine leere Liste, um die Karten zu speichern.
         cardList = []
 
+        # Ruft alle Karten der angegebenen Kategorie aus der Datenbank ab.
         for card in cls.db.getDataFromTableWithFilter("Cardholder", "category", category):
+            # Fügt jede Karte der Liste hinzu, indem eine Card-Instanz erstellt wird.
             cardList.append(Card(cardID=card["cardID"], question=card["question"], answer=card["answer"], category=card["category"]))
+        # Gibt die Liste der Karten zurück.
         return cardList
 
+    # Definiert eine Klassenmethode, die eine Karte aus einer Kartenliste extrahiert.
     @classmethod
     def extractOneCardFromCardList(cls, cardList):
+        # Überprüft, ob die Liste nicht leer ist.
         if len(cardList) > 0:
+            # Entfernt und gibt die letzte Karte aus der Liste zurück.
             card = cardList.pop()
             return card
+        # Gibt None zurück, wenn die Liste leer ist.
         return None
 
+    # Definiert eine Klassenmethode, die alle eindeutigen Kategorien zurückgibt.
     @classmethod
     def getAllCategories(cls):
+        # Ruft alle eindeutigen Kategorien aus der Datenbank ab.
         categories = cls.db.getAllUniqueValuesFromColumn("Cardholder", "category")
+        # Initialisiert eine leere Liste, um die Kategorien zu speichern.
         categoriesList = []
+        # Iteriert durch die Kategorien und fügt sie der Liste hinzu, wenn sie nicht bereits vorhanden sind.
         for category in categories:
             i = category["category"]
             if i not in categoriesList:
                 categoriesList.append(i)
+        # Gibt die Liste der Kategorien zurück.
         return categoriesList
-
-
-
