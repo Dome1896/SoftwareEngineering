b1d3e71d515974394c521692ed79ec0ffd62aa0a
dome1896 <dominikwalter2002@gmail.com>
2024-06-27 17:24:07 +0200

    cleaned up main file. Useable now

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-# Importiere die notwendigen Module aus Kivy, um eine einfache GUI-Anwendung zu erstellen.
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-
-# Importiere eigene Module, die zur Verwaltung von Datenbanken und Karten verwendet werden.
-from database import Database
-from card import Card
-from controller import Controller
 from frontend import LoginApp
 
+def main():
+    app = LoginApp()
+    app.run()
+
 if __name__ == "__main__":
-    LoginApp().run()
+    main()
 
